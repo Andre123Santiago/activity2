13d5a0cc2d5bfb4bd64800007d52c1bf76242479
Andre123Santiago <santiagoandre005@gmail.com>
2024-03-12 18:32:31 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>home</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="css/home.css" rel="stylesheet">
+    
+  </head>
+  <body class="p-3 m-0 border-0 bd-example m-0 border-0">
+  
+  
+    
+        
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" >NBA</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="about">About us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="contact">Contact</a>
+            </li>
+
+            <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Services
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Service1</a></li>
+            <li><a class="dropdown-item" href="#">Service2</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+          </ul>
+          <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
+      
+        </div>
+      </div>
+    </nav>
+
+
+     <div class="container-fluid text-center">
+
+            <h1> NBA LEGEND</h1> 
+           
+        </div>
+    <!-- End Example Code -->
+
+    <div class="sss">
+
+  <div class="col-container">
+  <div class="col">
+  <img class="qqq" src="image/curry.jpg" alt="">
+  </div>
+
+  <div class="col">
+  <img class="qqq" src="image/lebron.jpg" alt="">
+  </div>
+
+  <div class="col">
+  <img class="qqq" src="image/mj.jpg" alt="">
+  </div>
+</div>
+</div>
+<br><br><br><br><br><br><br><br><br><br>
+
+<footer id="footer">
+
+<p class="text">The National Basketball Association (NBA) is a professional basketball league in North America. </p> <br>
+   
+  </a>
+
+</footer>
+
+<br><br><br><br>
+
+
+
+<div id="contactus">
+
+        <div class="container-fluid pt-3 bg-dark">
+            <div class="row text-center">
+                <div class="col">
+                    <p class="text-white">Contact Us</p>
+                    <!-- <button class="btn success">Success</button> -->
+                    <a href=""><button type="button" class="btn btn-outline-primary">Facebook</button></a>
+                    <a href=""><button type="button" class="btn btn-outline-info">Twitter</button></a>
+                    <a href=""><button type="button" class="btn btn-outline-warning">Tiktok</button></a>
+                </div>
+            </div>
+        </div>
+
+</div>
+    
+
+
+
+  </body>
+</html>
